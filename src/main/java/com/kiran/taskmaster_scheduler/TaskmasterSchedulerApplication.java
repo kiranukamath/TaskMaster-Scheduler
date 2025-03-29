f05bce85d6dbd447fe13cd6b227adafb1d37ef41
Kiran U Kamath <kirankamat.mgm@gmail.com>
2025-03-29 10:44:30 +0530

    first commit

---
+++ src/main/java/com/kiran/taskmaster_scheduler/TaskmasterSchedulerApplication.java
@@ -0,0 +1,13 @@
+package com.kiran.taskmaster_scheduler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskmasterSchedulerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskmasterSchedulerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kiran/taskmaster_scheduler/TaskmasterSchedulerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kiran.taskmaster_scheduler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskmasterSchedulerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
